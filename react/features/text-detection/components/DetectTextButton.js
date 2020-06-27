d5a1cd38ae4c4a280d134b88550c30cf72012375
Kamil Ruta <4ruta4@gmail.com>
2020-06-27 14:55:10 +0200

    AWS API added

--- react/features/text-detection/components/DetectTextButton.js
+++ react/features/text-detection/components/DetectTextButton.js
@@ -3,6 +3,68 @@ import { AbstractButton, type AbstractButtonProps } from '../../base/toolbox';
 import { translate } from '../../base/i18n';
 import { IconHelp } from '../../base/icons';
 import { connect } from '../../base/redux';
+import AWS from 'aws-sdk';
+
+
+function getBlobFromMediaStream(stream) {
+
+    const video = document.createElement('video');
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    video.srcObject = stream;
+
+    return new Promise((resolve, reject) => {
+        try {
+            video.addEventListener('loadeddata', event => {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            video.play().then(() => {
+                context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+                canvas.toBlob(resolve, 'image/png');
+            });
+            });
+        } catch(error) {
+            reject(error);
+        }
+    });
+}
+
+
+function blobToBase64(blob) {
+    let uInt8Array = new Uint8Array(blob);
+    let i = uInt8Array.length;
+    var biStr = [];
+    while (i--) { biStr[i] = String.fromCharCode(uInt8Array[i]);  }
+    var base64 = btoa(biStr.join(''));
+    return base64;
+}
+
+
+function getText(base64) {
+    let rekognition = new AWS.Rekognition();
+    let params = {
+        Image: {
+            Bytes: base64
+        }
+        // ,
+        // Filters: {
+        //     MinConfidence: 50
+        // }
+    };
+
+    return new Promise((resolve, reject) => {
+        rekognition.detectText(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+                reject(err);
+            }
+            else
+                resolve(data);
+        });
+    });
+    
+}
 
 
 class DetectTextButton extends AbstractButton<AbstractButtonProps, *> {
@@ -17,13 +79,15 @@ class DetectTextButton extends AbstractButton<AbstractButtonProps, *> {
      * @returns {void}
      */
     _handleClick() {
-        console.log('new button clicked');
         let mediaStream = APP.UI.getLargeVideo().videoContainer.stream.stream;
-        let track = mediaStream.getVideoTracks()[0];
-        let imageCapture = new ImageCapture(track);
-        imageCapture.grabFrame().then(frame => {
-            console.log(frame);
+        getBlobFromMediaStream(mediaStream).then(async blob => {
+            let base64 = blobToBase64(blob);
+            let data = await getText(base64);
+            console.log(data);
         });
+
+        
+        console.log('new button clicked');
     }
 }
 
